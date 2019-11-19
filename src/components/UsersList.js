b210474bc2dd3ac4877ec8d9a6a94797aad0ed4c
Juan M. Garcia <juan@factoryfix.com>
2019-11-19 15:32:08 -0500

    [FIX] converting userList to an stateless component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import AppBar from './AppBar';
 import { INITIAL_USERS as topFive } from './../utils/constants';
 
-class UsersList extends Component {
-  state = {
-    topFive
-  }
-  redirectToPerson = (userName) => {
-    console.log(JSON.stringify(this.props))
-  }
-
-  render () {
-    return (
-      <React.Fragment>
-          <AppBar
-            currentPage="Home"
-          />
-          <div style={{padding: '20px'}}>
-            <h2>Top 5 GitHub Users</h2>
-            <p>Tab the user to see more information</p>
-            {this.state.topFive.map((user, index) => (
-              <Link
+function UsersList () {
+  return (
+    <React.Fragment>
+        <AppBar
+          currentPage="Home"
+        />
+        <div style={{padding: '20px'}}>
+          <h2>Top 5 GitHub Users</h2>
+          <p>Tab the user to see more information</p>
+          {topFive.map((user, index) => (
+            <Link
+              to={`person/${user}`}
+              key={index}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                style={{ margin: '5px', background: '#2196f3' }}
                 to={`person/${user}`}
-                key={index}
               >
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="large"
-                  style={{ margin: '5px', background: '#2196f3' }}
-                  to={`person/${user}`}
-                >
-                    { user }
-                </Button>
-              </Link>
-            ))}
-          </div>
-      </React.Fragment>
-    )
-  }
+                  { user }
+              </Button>
+            </Link>
+          ))}
+        </div>
+    </React.Fragment>
+  )
 }
 
 export default UsersList
